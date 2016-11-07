1bb5de4cb91cb49fdda0d5a091d945147716fc6f
leosongsong <sulisys@gmail.com>
2016-11-07 20:44:45 +0900

    HttpUtil

--- src/com/leo/utils/HttpUtil.java
+++ src/com/leo/utils/HttpUtil.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
  
 /**
+ * 
  * This class encapsulates methods for requesting a server via HTTP GET/POST and
  * provides methods for parsing response from the server.
  */
